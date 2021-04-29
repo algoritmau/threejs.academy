807a417ae907cacda3139a97850f61899d3e2748
Mauricio Paternina <spaceinvadev@gmail.com>
2021-04-29 17:48:44 -0500

    fix warning due to icon conditional error rendering on ThemeToggle

--- components/ThemeToggle.js
+++ components/ThemeToggle.js
@@ -1,8 +1,10 @@
 import { useTheme } from 'next-themes'
+import useMounted from '~/hooks/useMounted'
 import Icon from './Icon'
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useTheme()
+  const toggleIsMounted = useMounted()
 
   const handleClick = () => {
     setTheme(`${theme === 'light' ? 'dark' : 'light'}`)
@@ -11,10 +13,16 @@ export default function ThemeToggle() {
 
   return (
     <button
+      aria-label="Toggle Dark Mode"
+      type="button"
       className="w-5 h-5 p-0.5 flex justify-center items-center"
       onClick={handleClick}
     >
-      {theme === 'light' ? <Icon name="sun" /> : <Icon name="moon" />}
+      {toggleIsMounted && theme === 'light' ? (
+        <Icon name="sun" />
+      ) : (
+        <Icon name="moon" />
+      )}
     </button>
   )
 }
