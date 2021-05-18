a767e83fa6d3f811d7a0756e7dcf8d40927eef6a
Mauricio Paternina <spaceinvadev@gmail.com>
2021-05-18 13:10:54 -0500

    set <Layout>'s background colors

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Header from './Header'
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className="bg-white dark:bg-black">
       <Header />
       <main className="p-3">{children}</main>
       <Footer />
